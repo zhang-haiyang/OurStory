5e4e805fb21cfe389919861fea7dd62f166910d7
zhanghaiyang <2419378537@qq.com>
2018-01-16 17:16:52 +0800

    添加“言论”模块

---
+++ speech/src/main/java/zhy/com/speech/SsMainActivity.java
@@ -0,0 +1,13 @@
+package zhy.com.speech;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SsMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ss_main);
+    }
+}

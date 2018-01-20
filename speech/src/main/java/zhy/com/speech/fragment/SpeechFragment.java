1ba971f4920d2881d81ab9838ebcabc3c313d485
zhanghaiyang <2419378537@qq.com>
2018-01-20 20:30:35 +0800

    添加多媒体选择库PictureSelector：图片／gif／视频／拍照／摄像功能，并可以压缩图片

--- speech/src/main/java/zhy/com/speech/fragment/SpeechFragment.java
+++ speech/src/main/java/zhy/com/speech/fragment/SpeechFragment.java
@@ -1,13 +1,16 @@
 package zhy.com.speech.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import zhy.com.speech.R;
+import zhy.com.speech.activity.SsMainActivity;
 
 /**
  * 首页言论模块
@@ -17,11 +20,24 @@ import zhy.com.speech.R;
 public class SpeechFragment extends Fragment {
 
     private View mViewV;
+    private TextView mSpeechTV;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         mViewV = inflater.inflate(R.layout.fragment_speech, container, false);
+        initView();
         return mViewV;
+    }
+
+    private void initView(){
+        mSpeechTV = mViewV.findViewById(R.id.tv_speech);
+
+        mSpeechTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), SsMainActivity.class));
+            }
+        });
     }
 }
